b4f7bd5509c5425da22b2ceb384a94c8381bd30c
thanksdanny <hj-07@163.com>
2018-02-27 21:39:10 +0800

    Django 处理 HTTP 请求

--- td_blog/urls.py
+++ td_blog/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url, include # include的作用就是把blog应用下的urls.py包含进来
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
 ]
+
+
